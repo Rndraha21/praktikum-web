eae944ade56ff19a9bb3363d18cc3646c718cf92
Rndraha21 <robinndraha111@gmail.com>
2025-09-30 13:46:40 +0700

    tambahkan javascript

---
+++ script/script.js
@@ -0,0 +1,19 @@
+const msg = document.getElementById("msg");
+const btn = document.getElementById("btn-submit");
+
+const nama = document.getElementById("nama");
+const email = document.getElementById("email");
+const pesan = document.getElementById("pesan");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  msg.classList.toggle("hidden");
+  setTimeout(() => {
+    msg.classList.toggle("hidden");
+  }, 3000);
+
+  nama.value = "";
+  email.value = "";
+  pesan.value = "";
+});

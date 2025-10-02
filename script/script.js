d5c5cdf5d44092816895b4b7d9c8432143c4ac93
Rndraha21 <robinndraha111@gmail.com>
2025-10-02 07:16:36 +0700

    buat form lebih interaktif

--- script/script.js
+++ script/script.js
@@ -1,19 +1,25 @@
 const msg = document.getElementById("msg");
 const btn = document.getElementById("btn-submit");
 
-const nama = document.getElementById("nama");
-const email = document.getElementById("email");
-const pesan = document.getElementById("pesan");
-
 btn.addEventListener("click", (e) => {
   e.preventDefault();
 
+  const nama = document.getElementById("nama").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const pesan = document.getElementById("pesan").value.trim();
+
   msg.classList.toggle("hidden");
+
+  if (nama != "" && email != "" && pesan != "") {
+    msg.textContent = "Pesan terkirim";
+    document.getElementById("nama").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("pesan").value = "";
+  } else {
+    msg.textContent = "Silahkan isi semua form terlebih dahulu";
+  }
+
   setTimeout(() => {
     msg.classList.toggle("hidden");
-  }, 5000);
-
-  nama.value = "";
-  email.value = "";
-  pesan.value = "";
+  }, 3000);
 });
